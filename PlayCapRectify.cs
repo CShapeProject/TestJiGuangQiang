3ff1f5e1c353507458f5b67d04a590160d436ea8
syq <shenyongqiang2008@126.com>
2017-05-16 15:01:52 +0800

    取消opencv的引用

--- PlayCapRectify.cs
+++ PlayCapRectify.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Runtime.InteropServices;
 using DirectShowLib;
-using OpenCvSharp;
 using System.Windows.Forms;
 using System.Runtime.InteropServices.ComTypes;
 
