2b72c764949dd7d1fab580a5a1811c23def94c4f
syq <shenyongqiang2008@126.com>
2017-05-16 15:03:38 +0800

    添加光枪坐标信息和UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -11,14 +11,15 @@
         /// 清理所有正在使用的资源。
         /// </summary>
         /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing && (components != null))
+        //    {
+        //        components.Dispose();
+        //    }
+
+        //    base.Dispose(disposing);
+        //}
 
         #region Windows 窗体设计器生成的代码
 
@@ -28,20 +29,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.JiaoZhunPic = new System.Windows.Forms.PictureBox();
+            this.ZhunXingZB = new System.Windows.Forms.Label();
+            this.ZhunXingP1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.JiaoZhunPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ZhunXingP1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // JiaoZhunPic
+            // 
+            this.JiaoZhunPic.Location = new System.Drawing.Point(0, 0);
+            this.JiaoZhunPic.Name = "JiaoZhunPic";
+            this.JiaoZhunPic.Size = new System.Drawing.Size(1360, 768);
+            this.JiaoZhunPic.TabIndex = 0;
+            this.JiaoZhunPic.TabStop = false;
+            // 
+            // ZhunXingZB
+            // 
+            this.ZhunXingZB.Font = new System.Drawing.Font("宋体", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ZhunXingZB.Location = new System.Drawing.Point(67, 59);
+            this.ZhunXingZB.Name = "ZhunXingZB";
+            this.ZhunXingZB.Size = new System.Drawing.Size(500, 50);
+            this.ZhunXingZB.TabIndex = 1;
+            this.ZhunXingZB.Text = "px: 1360 py: 768";
+            // 
+            // ZhunXingP1
+            // 
+            this.ZhunXingP1.BackColor = System.Drawing.SystemColors.Control;
+            this.ZhunXingP1.Image = global::OpenCameraCSByOpenCV.Properties.Resources.ZhunXingP1;
+            this.ZhunXingP1.Location = new System.Drawing.Point(165, 166);
+            this.ZhunXingP1.Name = "ZhunXingP1";
+            this.ZhunXingP1.Size = new System.Drawing.Size(165, 166);
+            this.ZhunXingP1.TabIndex = 2;
+            this.ZhunXingP1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(472, 326);
+            this.ClientSize = new System.Drawing.Size(1357, 766);
+            this.Controls.Add(this.ZhunXingP1);
+            this.Controls.Add(this.ZhunXingZB);
+            this.Controls.Add(this.JiaoZhunPic);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.JiaoZhunPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ZhunXingP1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox JiaoZhunPic;
+        private System.Windows.Forms.Label ZhunXingZB;
+        private System.Windows.Forms.PictureBox ZhunXingP1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,17 +6,146 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace OpenCameraCSByOpenCV
 {
     public partial class Form1 : Form
     {
+        Thread m_FormThread;
+        bool IsExitApp;
+        Point ZhunXingPoint;
+
+        public static Form1 Instance;
+        //注册热键的api.
+        [DllImport("user32")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint control, Keys vk);
+        //解除注册热键的api.
+        [DllImport("user32")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
         public Form1()
         {
             InitializeComponent();
+            ZhunXingZB.Text = "px: 0 py: 0";
+            Instance = this;
 
             pcvr pcvrPtr = new pcvr();
             pcvrPtr.createCamera();
+
+            RegisterHotKey(this.Handle, (int)Keys.F4, 0, Keys.F4);
+            RegisterHotKey(this.Handle, (int)Keys.K, 0, Keys.K);
+
+            Control.CheckForIllegalCrossThreadCalls = false;
+            m_FormThread = new Thread(ShowFormInfo);
+            m_FormThread.IsBackground = true;
+            m_FormThread.Start();  
+        }
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (m_FormThread.IsAlive)
+            {
+                m_FormThread.Abort();
+            }
+            IsExitApp = true;
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x0312:    //这个是window消息定义的,注册的热键消息.
+                    if (m.WParam.ToInt32() == (int)Keys.F4)
+                    {
+                        InitJiaoZhunZuoBiao();
+                    }
+
+                    if (m.WParam.ToInt32() == (int)Keys.K)
+                    {
+                        if (JiaoZhunPic.Visible)
+                        {
+                            pcvr.m_CamCB.ActiveJiaoZhunZuoBiao();
+                        }
+                    }
+                    break;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        void InitJiaoZhunZuoBiao()
+        {
+            if (JiaoZhunPic.Image != null)
+            {
+                return;
+            }
+            //MessageBox.Show("开始校准坐标!");
+            ZhunXingZB.Visible = false;
+            ZhunXingP1.Visible = false;
+            ChangeJiaoZhunPic(1);
+            pcvr.m_CamCB.InitJiaoZhunZuoBiao();
+        }
+
+        public void ChangeJiaoZhunPic(byte indexVal)
+        {
+            //Console.WriteLine("indexVal " + indexVal);
+            switch (indexVal)
+            {
+                case 1:
+                    JiaoZhunPic.Image = global::OpenCameraCSByOpenCV.Properties.Resources.GunJY_0;
+                    break;
+                case 2:
+                    JiaoZhunPic.Image = global::OpenCameraCSByOpenCV.Properties.Resources.GunJY_1;
+                    break;
+                case 3:
+                    JiaoZhunPic.Image = global::OpenCameraCSByOpenCV.Properties.Resources.GunJY_2;
+                    break;
+                case 4:
+                    JiaoZhunPic.Image = global::OpenCameraCSByOpenCV.Properties.Resources.GunJY_3;
+                    break;
+                default:
+                    JiaoZhunPic.Image = null;
+                    ZhunXingZB.Visible = true;
+                    ZhunXingP1.Visible = true;
+                    break;
+            }
+        }
+
+        public void UpdateZhunXingZuoBiao(Point pointVal)
+        {
+            int px = pointVal.X;
+            px = px > 1360 ? 1360 : px;
+            px = px < 0 ? 0 : px;
+
+            int py = 768 - pointVal.Y;
+            py = py > 768 ? 768 : py;
+            py = py < 0 ? 0 : py;
+            ZhunXingPoint = new Point(px, py);
+        }
+
+        void ShowFormInfo()
+        {
+            do
+            {
+                if (IsExitApp)
+                {
+                    break;
+                }
+                ZhunXingZB.Text = "px: " + ZhunXingPoint.X.ToString("d4") + " py: " + ZhunXingPoint.Y.ToString("d3");
+                ZhunXingP1.Location = new Point(ZhunXingPoint.X, ZhunXingPoint.Y);
+                Thread.Sleep(10);
+            } while (!IsExitApp);
+            Console.WriteLine("Exit thread!");
         }
     }
 }
