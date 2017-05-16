b80e45d9e2351d33a4ebef5dedbe1994f6494bae
syq <shenyongqiang2008@126.com>
2017-05-16 15:06:43 +0800

    删除无用代码

--- pcvr.cs
+++ pcvr.cs
@@ -8,27 +8,11 @@ namespace OpenCameraCSByOpenCV
 {
     class pcvr
     {
-        CSampleGrabberCB m_CamCB = null;
+        public static CSampleGrabberCB m_CamCB = null;
         public void createCamera()
         {
             m_CamCB = new CSampleGrabberCB(0);
             m_CamCB.Start();
-            //PlayCapRectify playCap = new PlayCapRectify();
-            //playCap.CaptureVideo();
-
-
-            //if( playCap.RunGun() )
-            //{
-            //    CSampleGrabberCB CSgcb1 = playCap.CB;
-            //    CSgcb1.m_mode = MODE.MODE_MOTION;
-            //    playCap.PlayVideo();
-            //    //mCameraEnable = true;
-            //    //setCamPointCallBackFun();
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Can not find Camera!");
-            //}
         }
     }
 }
